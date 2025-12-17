d2516733215b5898dd5c13ec3116a11ec54ef6c9
Prashant-kumar-sharma <prashant10112003@gmail.com>
2025-12-17 17:08:23 +0530

    [Prashant Kumar Sharma] : added program to convert kilometres to miles

---
+++ gcr-codebase/programming-elements/level 1/KilometresToMiles.java
@@ -0,0 +1,14 @@
+public class KilometresToMiles {
+    public static void main(String[] args) {
+        //Creating variables for distance values
+        double kilometres, miles;
+
+        //Assigning kilometres distance value
+        kilometres = 10.8;
+        //Converting kilometres to miles
+        miles = kilometres * 1.6;
+
+        //Displaying the results
+        System.out.println("The distance " + kilometres +" km in miles is " + miles);
+    }
+}
