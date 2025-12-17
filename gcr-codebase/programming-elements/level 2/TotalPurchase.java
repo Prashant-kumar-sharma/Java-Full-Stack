d60ac08d8ad9ac0d72c8bf0a982ca6118d03bb2e
Prashant-kumar-sharma <prashant10112003@gmail.com>
2025-12-17 21:20:34 +0530

    [Prashant Kumar Sharma] : added program to calculate total purchase from unit price and quantity

---
+++ gcr-codebase/programming-elements/level 2/TotalPurchase.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class TotalPurchase {
+    public static void main(String[] args) {
+        //Creating Scanner object to take input from user
+        Scanner input = new Scanner(System.in);
+        double unitPrice, quantity;
+        double totalPrice;
+	
+        //Input and calculation
+        unitPrice = input.nextDouble();
+        quantity = input.nextDouble();
+        totalPrice = unitPrice * quantity; 
+
+	//displaying the results
+        System.out.println("The total purchase price is INR " + totalPrice + " if the quantity is " + quantity + " and unit price is INR " + unitPrice);
+	input.close(); //Closing the Scanner object
+    }
+}
