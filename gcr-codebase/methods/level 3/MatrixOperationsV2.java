0f23c1365f210c5bd8610dba0a1c715a0a0fd888
Prashant-kumar-sharma <prashant10112003@gmail.com>
2025-12-23 21:43:04 +0530

    [Prashant Kumar Sharma] : added inverse3x3 method to MatrixOperationsV2 java file

--- gcr-codebase/methods/level 3/MatrixOperationsV2.java
+++ gcr-codebase/methods/level 3/MatrixOperationsV2.java
@@ -65,10 +65,34 @@ public class MatrixOperationsV2 {
         return inverse;
     }
 
+    // Method to find inverse of 3x3 matrix
+    public static double[][] inverse3X3(int[][] matrix) {
+        int det = determinant3X3(matrix);
+        if (det == 0) {
+            System.err.println("Inverse does not exist (Determinant = 0)");
+            return null;
+        }
+        double[][] inverse = new double[3][3];
+        // Cofactor matrix (then transposed = adjugate)
+        inverse[0][0] =  (matrix[1][1]*matrix[2][2] - matrix[1][2]*matrix[2][1]) / (double) det;
+        inverse[0][1] = -(matrix[0][1]*matrix[2][2] - matrix[0][2]*matrix[2][1]) / (double) det;
+        inverse[0][2] =  (matrix[0][1]*matrix[1][2] - matrix[0][2]*matrix[1][1]) / (double) det;
+
+        inverse[1][0] = -(matrix[1][0]*matrix[2][2] - matrix[1][2]*matrix[2][0]) / (double) det;
+        inverse[1][1] =  (matrix[0][0]*matrix[2][2] - matrix[0][2]*matrix[2][0]) / (double) det;
+        inverse[1][2] = -(matrix[0][0]*matrix[1][2] - matrix[0][2]*matrix[1][0]) / (double) det;
+
+        inverse[2][0] =  (matrix[1][0]*matrix[2][1] - matrix[1][1]*matrix[2][0]) / (double) det;
+        inverse[2][1] = -(matrix[0][0]*matrix[2][1] - matrix[0][1]*matrix[2][0]) / (double) det;
+        inverse[2][2] =  (matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0]) / (double) det;
+
+        return inverse;
+    }
+
     public static void main(String[] args) {
         int[][] matrix = createRandomMatrix(2, 2);
 
-        System.out.println("Original Matrix:");
+        System.out.println("Original 2x2 Matrix:");
         displayMatrix(matrix);
 
         System.out.println("\nTranspose:");
@@ -78,12 +102,29 @@ public class MatrixOperationsV2 {
         System.out.println("\nDeterminant: " + det);
 
         System.out.println("\nInverse:");
-        double[][] inverse = inverse2X2(matrix);
+        double[][] inverse2x2 = inverse2X2(matrix);
         
-        if (inverse != null) {
-            for (int i = 0; i < inverse.length; i++) {
-                for (int j = 0; j < inverse[0].length; j++) {
-                    System.out.printf("%.2f ", inverse[i][j]);
+        if (inverse2x2 != null) {
+            for (int i = 0; i < inverse2x2.length; i++) {
+                for (int j = 0; j < inverse2x2[0].length; j++) {
+                    System.out.printf("%.2f ", inverse2x2[i][j]);
+                }
+                System.out.println();
+            }
+        }
+
+        int[][] matrix3 = createRandomMatrix(3, 3);
+
+        System.out.println("Original 3x3 Matrix:");
+        displayMatrix(matrix3);
+
+        double[][] inverse3x3 = inverse3X3(matrix3);
+
+        if (inverse3x3 != null) {
+            System.out.println("\nInverse:");
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    System.out.printf("%.2f ", inverse3x3[i][j]);
                 }
                 System.out.println();
             }
