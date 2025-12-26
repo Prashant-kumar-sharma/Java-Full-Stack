b7eae40541da4b474dd1800f1bb6ef5fff8e2c47
Prashant Sharma <prashant10112003@google.com>
2025-12-26 15:13:07 +0530

    [Prashant Kumar Sharma] : Added LeetCode solution for Longest Common Prefix problem

---
+++ leet-code-codebase/arrays/LC0014_LongestCommonPrefix.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class LC0014_LongestCommonPrefix {
+
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+
+        String pref = strs[0];
+        int prefLen = pref.length();
+
+        for (int i = 1; i < strs.length; i++) {
+            String s = strs[i];
+            while (prefLen > s.length() || !pref.equals(s.substring(0, prefLen))) {
+                prefLen--;
+                if (prefLen == 0) {
+                    return "";
+                }
+                pref = pref.substring(0, prefLen);
+            }
+        }
+
+        return pref;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        LC0014_LongestCommonPrefix sol = new LC0014_LongestCommonPrefix();
+
+        System.out.print("Enter number of strings: ");
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        String[] strs = new String[n];
+        System.out.println("Enter strings:");
+        for (int i = 0; i < n; i++) {
+            strs[i] = sc.nextLine();
+        }
+
+        System.out.println("Longest Common Prefix: " + sol.longestCommonPrefix(strs));
+    }
+}
