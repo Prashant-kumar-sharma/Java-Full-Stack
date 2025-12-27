019c506ef2466920c1e4947386e7cd90d0ae52bd
Prashant-kumar-sharma <prashant10112003@gmail.com>
2025-12-27 23:41:32 +0530

    [Prashant Kumar Sharma] : added Vehicle java file

---
+++ gcr-codebase/JavaOopsConcepts/src/com/constructors/levelone/Vehicle.java
@@ -0,0 +1,41 @@
+package com.constructors.levelone;
+
+public class Vehicle {
+	// Instance variables
+	private String ownerName;
+	private String vehicleType;
+
+	// Class variable
+	private static double registrationFee = 5000;
+
+	// Initializes vehicle details
+	public Vehicle(String ownerName, String vehicleType) {
+		this.ownerName = ownerName;
+		this.vehicleType = vehicleType;
+	}
+
+	// Displays vehicle details
+	public void displayVehicleDetails() {
+		System.out.println("Owner Name: " + ownerName);
+		System.out.println("Vehicle Type: " + vehicleType);
+		System.out.println("Registration Fee: " + registrationFee);
+	}
+
+	// Updates registration fee for all vehicles
+	public static void updateRegistrationFee(double newFee) {
+		registrationFee = newFee;
+	}
+
+	public static void main(String[] args) {
+		Vehicle v1 = new Vehicle("Rohit", "Car");
+		Vehicle v2 = new Vehicle("Anita", "Bike");
+
+		v1.displayVehicleDetails();
+		v2.displayVehicleDetails();
+
+		Vehicle.updateRegistrationFee(6000);
+
+		v1.displayVehicleDetails();
+		v2.displayVehicleDetails();
+	}
+}
