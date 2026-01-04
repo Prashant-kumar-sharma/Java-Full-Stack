cf662cf1267bea243998bc3ba4a3538b629badef
Prashant Sharma <prashant10112003@google.com>
2026-01-05 01:37:46 +0530

    [Prashant Kumar Sharma] : Added scenario-problem statement to the solution.

--- scenario-based-codebase/ScenarioPractice/src/com/day02/mybankapplication/MyBankApplication.java
+++ scenario-based-codebase/ScenarioPractice/src/com/day02/mybankapplication/MyBankApplication.java
@@ -1,3 +1,18 @@
+/**
+* 7. "MyBank – Managing Customer Accounts"
+* Story: You're building the core banking module for MyBank, a digital-first bank. 
+* Your job is to manage customer accounts, deposits, and interest calculations.
+* Requirements:
+* Account (base class): fields like accountNumber, balance.
+* Derived classes: SavingsAccount, CurrentAccount (using inheritance).
+* ITransaction interface: deposit(), withdraw(), checkBalance().
+* Use constructors for account initialization with or without an opening balance.
+* Encapsulation: balance must be private and only modifiable through methods.
+* Use operators for interest: balance * interestRate / 100.
+* Polymorphism in calculateInterest() – varies by account type.
+* Use access modifiers: protect account data, expose only required operations.
+*/
+
 package com.day02.mybankapplication;
 
 import java.util.Scanner;
