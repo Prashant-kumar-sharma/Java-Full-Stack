5f1cc87a849343eaf625f483a0872ce88730d888
Prashant Sharma <prashant10112003@google.com>
2026-01-07 21:32:28 +0530

    [Prashant Kumar Sharma] : Added problem statement for PetrPal scenario based problem.

--- scenario-based-codebase/ScenarioPractice/src/com/day04/petpalapplication/PetPalApplication.java
+++ scenario-based-codebase/ScenarioPractice/src/com/day04/petpalapplication/PetPalApplication.java
@@ -1,3 +1,18 @@
+/*
+12. "PetPal – Virtual Pet Care App"
+Story: You’re creating PetPal, an app that allows users to adopt and care for virtual pets like
+dogs, cats, and birds.
+Requirements:
+● Pet base class: name, type, age.
+● Derived classes: Dog, Cat, Bird (use inheritance).
+● IInteractable interface with methods like feed(), play(), sleep().
+● Encapsulation of hunger and mood levels.
+● Use constructors to assign random default values or user-specified pets.
+● Operators to increase/decrease energy, hunger based on interactions.
+● Polymorphism: makeSound() behaves differently for each pet.
+● Access modifiers ensure internal mood status is only modifiable through methods.
+*/
+
 package com.day04.petpalapplication;
 
 import java.util.Scanner;
