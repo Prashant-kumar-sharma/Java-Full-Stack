a5927d290dd0ee4f854af6e17b097938f4eb6642
Prashant-kumar-sharma <prashant10112003@gmail.com>
2026-01-07 21:32:14 +0530

    [Prashant Kumar Sharma] : Added problem statement for FitTrack scenario based problem.

--- scenario-based-codebase/ScenarioPractice/src/com/day04/fittrackapplication/FitTrackApplication.java
+++ scenario-based-codebase/ScenarioPractice/src/com/day04/fittrackapplication/FitTrackApplication.java
@@ -1,3 +1,20 @@
+/*
+11. "FitTrack – Your Personal Fitness Tracker"
+Story: You’ve been hired to develop FitTrack, a fitness tracking app that monitors users'
+workouts, calories, and daily progress.
+Requirements:
+● UserProfile class with fields like name, age, weight, goal.
+● Workout class to track type, duration, caloriesBurned.
+● Constructors to allow user creation with default or custom goals.
+● ITrackable interface with startWorkout() and stopWorkout() methods.
+● Use encapsulation to protect private health data (e.g., weight).
+● Operators to compute calorie targets and progress (dailyTarget -
+caloriesBurned).
+● Inheritance: CardioWorkout, StrengthWorkout extend Workout.
+● Polymorphism: calculateCalories() differs by workout type.
+● Access modifiers to restrict internal workout logs from being exposed directly.
+*/
+
 package com.day04.fittrackapplication;
 
 import java.util.Scanner;
