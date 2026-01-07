00ee1cb9537115b06d263e01c93e393268000b79
Prashant-kumar-sharma <prashant10112003@gmail.com>
2026-01-07 21:31:30 +0530

    [Prashant Kumar Sharma] : Added problem statement for EduMentor scenario based problem.

--- scenario-based-codebase/ScenarioPractice/src/com/day04/edumentorapplication/EduMentorApplication.java
+++ scenario-based-codebase/ScenarioPractice/src/com/day04/edumentorapplication/EduMentorApplication.java
@@ -1,3 +1,21 @@
+/*
+10. "EduMentor – Personalized Learning Platform"
+Story: EduMentor is an AI-based learning platform. You're tasked with implementing the module
+to manage courses, quizzes, and learner progress.
+
+Requirements:
+● User base class: name, email, userId.
+● Learner and Instructor inherit from User.
+● ICertifiable interface with method generateCertificate().
+● Quiz class: fields for questions, answers, and score.
+● Use constructors to create quizzes with variable difficulty.
+● Encapsulation for quiz answers (can’t be modified once set).
+● Use operators to score the quiz and generate a percentage.
+● Polymorphism: generateCertificate() works differently for short courses vs
+full-time.
+● Apply access modifiers: internal question banks must remain private.
+*/
+
 package com.day04.edumentorapplication;
 
 import java.util.Scanner;
