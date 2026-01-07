b4ee6c3b684100b2a4724ee2c669c0ceae97e5d5
Prashant-kumar-sharma <prashant10112003@gmail.com>
2026-01-07 21:31:59 +0530

    [Prashant Kumar Sharma] : Added problem statement for EventEase scenario based problem.

--- scenario-based-codebase/ScenarioPractice/src/com/day04/eventeaseapplication/EventEaseApplication.java
+++ scenario-based-codebase/ScenarioPractice/src/com/day04/eventeaseapplication/EventEaseApplication.java
@@ -1,3 +1,21 @@
+/*
+13. "EventEase – Event Management Platform"
+Story: EventEase helps users organize events like birthdays, weddings, and conferences.
+You’re responsible for the booking and scheduling module.
+Requirements:
+● Event class: eventName, location, date, attendees.
+● User class to represent the organizer.
+
+Scenario-based Problems
+● Constructors to create events with or without catering/decoration packages.
+● ISchedulable interface with methods schedule(), reschedule(), cancel().
+● Use access modifiers to restrict editing of eventId once assigned.
+● Use operators to calculate costs (venue + services – discounts).
+● Inheritance: BirthdayEvent, ConferenceEvent extend Event.
+● Polymorphism: schedule() behaves differently based on event type.
+● Apply encapsulation to protect pricing and sensitive user data.
+*/
+
 package com.day04.eventeaseapplication;
 
 import java.util.Scanner;
