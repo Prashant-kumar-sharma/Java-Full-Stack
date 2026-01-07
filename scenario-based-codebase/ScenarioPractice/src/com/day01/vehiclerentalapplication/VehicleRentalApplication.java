4bb5313cb06412163156505ca974d31b1c9ea2c1
Prashant Sharma <prashant10112003@google.com>
2026-01-07 22:25:16 +0530

    [Prashant Kumar Sharma] : Added problem statement to VehicleRental scenario based problem.

--- scenario-based-codebase/ScenarioPractice/src/com/day01/vehiclerentalapplication/VehicleRentalApplication.java
+++ scenario-based-codebase/ScenarioPractice/src/com/day01/vehiclerentalapplication/VehicleRentalApplication.java
@@ -1,3 +1,21 @@
+/*
+2. Vehicle Rental Application
+Scenario: Build a system for renting bikes, cars, and trucks to customers.
+Concepts Used:
+● Class: Vehicle, Bike, Car, Truck, Customer
+
+Scenario-based Problems
+● Constructors: Custom constructors for different vehicle types
+● Access Modifiers: protected for vehicle attributes to allow subclass access
+● Interface: Rentable with method calculateRent(int days)
+● Operators: Used in rent calculation (e.g., baseRate × days ± surcharges)
+● OOP:
+○ Abstraction: Rentable abstracts rental behavior
+○ Inheritance: Car, Bike, Truck inherit from Vehicle
+○ Polymorphism: Rent calculation differs by vehicle type
+○ Encapsulation: All fields hidden behind getters/setters
+*/
+
 package com.day01.vehiclerentalapplication;
 
 public class VehicleRentalApplication {
