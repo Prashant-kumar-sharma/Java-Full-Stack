f22eb24b5e5880228e004aa36ffff60120addf25
Prashant-kumar-sharma <prashant10112003@gmail.com>
2026-01-07 22:24:28 +0530

    [Prashant Kumar Sharma] : Added problem statement to SmartHomeAutomationSystem scenario based problem.

--- scenario-based-codebase/ScenarioPractice/src/com/day01/smarthomeautomationsystem/UserController.java
+++ scenario-based-codebase/ScenarioPractice/src/com/day01/smarthomeautomationsystem/UserController.java
@@ -1,3 +1,22 @@
+/*
+5. Smart Home Automation System
+Scenario: Users can control appliances like lights, fans, ACs via a smart interface.
+Concepts Used:
+● Class: Appliance, Light, Fan, AC, UserController
+
+Scenario-based Problems
+
+● Constructor: Set default or user-defined power settings
+● Access Modifiers: Internal device settings as private, toggle methods as public
+● Interface: Controllable with methods turnOn(), turnOff()
+● Operators: Compare energy usage (>, <, ==)
+● OOP:
+○ Encapsulation: Control internal appliance state
+○ Abstraction: Devices follow common control interface
+○ Inheritance: Device types inherit from Appliance
+○ Polymorphism: Turning on a Light vs an AC involves different behaviors
+*/
+
 package com.day01.smarthomeautomationsystem;
 
 public class UserController {
