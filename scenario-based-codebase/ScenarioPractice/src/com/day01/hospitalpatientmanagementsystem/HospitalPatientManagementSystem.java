e866c566f37df42a068fa03d00ed917cec4dee8c
Prashant Sharma <prashant10112003@google.com>
2026-01-07 22:23:48 +0530

    [Prashant Kumar Sharma] : Added problem statement to HospitalPatientManagementSystem scenario based problem.

--- scenario-based-codebase/ScenarioPractice/src/com/day01/hospitalpatientmanagementsystem/HospitalPatientManagementSystem.java
+++ scenario-based-codebase/ScenarioPractice/src/com/day01/hospitalpatientmanagementsystem/HospitalPatientManagementSystem.java
@@ -1,3 +1,21 @@
+/*
+1. Hospital Patient Management System
+Scenario: A hospital wants a software module to register patients, assign doctors, and maintain
+billing.
+Concepts Used:
+● Class: Patient, Doctor, Bill
+● Constructor: Overloaded for emergency vs. normal patient admission
+● Access Modifiers: Keep sensitive data like medicalHistory as private, expose
+getSummary() via public
+● Interface: Payable with method calculatePayment() implemented in Bill
+● Operators: Used to compute discounts, taxes, totals
+● OOP:
+○ Encapsulation: Private fields with getter/setters
+○ Abstraction: Hide billing calculation behind interface
+○ Inheritance: InPatient, OutPatient extend Patient
+○ Polymorphism: displayInfo() overridden for Doctor, Patient, etc.
+*/
+
 package com.day01.hospitalpatientmanagementsystem;
 
 public class HospitalPatientManagementSystem {
